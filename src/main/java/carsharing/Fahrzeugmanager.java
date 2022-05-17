95d7eb0f7671ff8ac271cdb957da3ef3722fb330
Strozzii <stroz.dennis@gmail.com>
2022-05-17 16:51:29 +0200

    Fahrzeugmanager und Fahrzeugmanagertest zuende

--- src/main/java/carsharing/Fahrzeugmanager.java
+++ src/main/java/carsharing/Fahrzeugmanager.java
@@ -1,6 +1,9 @@
 package carsharing;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class Fahrzeugmanager {
 
@@ -12,11 +15,48 @@ public class Fahrzeugmanager {
         }
     }
 
+    public ArrayList<String> gibFahrzeugnamen(){
+        ArrayList<String> output = new ArrayList<>(fahrzeuge.keySet());
+        Collections.sort(output);
+        return output;
+    }
+
+    public ArrayList<String> gibFahrzeugnamen(String ort){
+        ArrayList<String> output = new ArrayList<>();
+        Iterator<Fahrzeug> iter = fahrzeuge.values().iterator();
+
+        while(iter.hasNext()){
+            Fahrzeug tmp = iter.next();
+            if(tmp.gibStandort().equals(ort)){
+                output.add(tmp.gibName());
+            }
+
+        }
+        Collections.sort(output);
+        return output;
+
+    }
+
     public boolean bucheFahrzeug(String fahrzeug, String anfang, String ende){
         Buchung b = new Buchung(anfang, ende);
         fahrzeuge.get(fahrzeug).buche(b);
 
         return fahrzeuge.get(fahrzeug).pruefeFrei(b);
+    }
+
+    public ArrayList<String> gibVerfuegbareFahrzeuge(String ort, String start, String ende){
+        ArrayList<String> output = new ArrayList<>();
+        Iterator<Fahrzeug> iterValues = fahrzeuge.values().iterator();
+
+        Buchung b = new Buchung (start, ende);
+        while(iterValues.hasNext()){
+            Fahrzeug tmp = iterValues.next();
+            if(tmp.gibStandort() == ort && tmp.pruefeFrei(b)){
+                output.add(tmp.gibName());
+            }
+        }
+        Collections.sort(output);
+        return output;
     }
 
 }
--- src/main/java/carsharing/FahrzeugmanagerTest.java
+++ src/main/java/carsharing/FahrzeugmanagerTest.java
@@ -1,6 +1,9 @@
 package carsharing;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FahrzeugmanagerTest {
 
@@ -13,10 +16,96 @@ public class FahrzeugmanagerTest {
         test.fuegeFahrzeugHinzu("Bahnhof2", "Bahnhof");
         test.fuegeFahrzeugHinzu("Bahnhof3", "Bahnhof");
 
-        test.bucheFahrzeug("Bahnhof1", "14.April 2005, 20:00 Uhr", "15.April 2005, 08:00 Uhr");
-        test.bucheFahrzeug("Bahnhof1", "15.April 2005, 18:00 Uhr", "16.April 2005, 00:00 Uhr");
-        test.bucheFahrzeug("Bahnhof2", "14.April 2005, 11:00 Uhr", "15.April 2005, 12:00 Uhr");
-        test.bucheFahrzeug("Bahnhof3", "15.April 2005, 10:00 Uhr", "15.April 2005, 19:00 Uhr");
+        test.bucheFahrzeug("Bahnhof1", "2005/04/14 20:00", "2005/04/15 08:00");
+        test.bucheFahrzeug("Bahnhof1", "2005/04/15 18:00", "2005/04/16 00:00");
+        test.bucheFahrzeug("Bahnhof2", "2005/04/14 11:00", "2005/04/15 12:00");
+        test.bucheFahrzeug("Bahnhof3", "2005/04/15 10:00", "2005/04/15 19:00");
+    }
+
+    @Test
+    void testGibFahrzeugnamen1(){
+        ArrayList<String> soll = new ArrayList<>();
+
+        soll.add("Bahnhof1");
+        soll.add("Bahnhof2");
+        soll.add("Bahnhof3");
+        soll.add("Rathaus1");
+
+        assertEquals(soll, test.gibFahrzeugnamen());
+    }
+
+    @Test
+    void testGibFahrzeugnamen2(){
+        ArrayList<String> soll = new ArrayList<>();
+
+        soll.add("Bahnhof1");
+        soll.add("Bahnhof2");
+        soll.add("Bahnhof3");
+
+        assertEquals(soll, test.gibFahrzeugnamen("Bahnhof"));
+    }
+
+    @Test
+    void testGibVerfuegbareFahrzeuge1() {
+        ArrayList<String> soll = new ArrayList<>();
+
+        assertEquals(soll, test.gibVerfuegbareFahrzeuge("Bahnhof", "2005/04/15 11:30", "2005/04/15 19:00"));
+    }
+
+    @Test
+    void testGibVerfuegbareFahrzeuge2() {
+        ArrayList<String> soll = new ArrayList<>();
+        soll.add("Bahnhof1");
+        soll.add("Bahnhof2");
+
+        assertEquals(soll, test.gibVerfuegbareFahrzeuge("Bahnhof", "2005/04/15 12:00", "2005/04/15 18:00"));
+    }
+
+    @Test
+    void testGibVerfuegbareFahrzeuge3() {
+        ArrayList<String> soll = new ArrayList<>();
+        soll.add("Bahnhof2");
+        soll.add("Bahnhof3");
+
+        assertEquals(soll, test.gibVerfuegbareFahrzeuge("Bahnhof", "2005/04/15 19:15", "2005/04/15 23:00"));
+    }
+
+    @Test
+    void testBucheFahrzeug1() {
+
+        assertTrue(test.bucheFahrzeug("Bahnhof 3", "2005/04/15 09:00", "2005/04/15 10:00"));
+    }
+
+    @Test
+    void testBucheFahrzeug2() {
+
+        test.bucheFahrzeug("Bahnhof 3", "2005/04/15 09:00", "2005/04/15 10:00");
+        assertFalse(test.bucheFahrzeug("Bahnhof 3", "2005/04/15 09:00", "2005/04/15 11:00"));
+    }
+
+    @Test
+    void testBucheFahrzeug3() {
+
+        assertFalse(test.bucheFahrzeug("Bahnhof 3", "2005/04/15 11:00", "2005/04/15 18:00"));
+    }
+
+    @Test
+    void testBucheFahrzeug4() {
+
+        assertFalse(test.bucheFahrzeug("Bahnhof 3", "2005/04/15 18:00", "2005/04/15 20:00"));
+    }
+
+    @Test
+    void testBucheFahrzeug5() {
+
+        assertTrue(test.bucheFahrzeug("Bahnhof 3", "2005/04/15 19:00", "2005/04/15 20:00"));
+    }
+
+    @Test
+    void testBucheFahrzeug6() {
+
+        test.bucheFahrzeug("Bahnhof 3", "2005/04/15 19:00", "2005/04/15 20:00");
+        assertFalse(test.bucheFahrzeug("Bahnhof 3", "2005/04/15 09:00", "2005/04/15 20:00"));
     }
 
 
